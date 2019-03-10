e06ec664829dfb7be0c78909df849f71acdaa783
minyoungpark1 <48391620+minyoungpark1@users.noreply.github.com>
2019-03-10 14:43:25 -0500

    Update make_noisy_dataset.py
    Added copying transcript method

--- make_noisy_dataset.py
+++ make_noisy_dataset.py
@@ -15,8 +15,10 @@ import os, fnmatch, shutil
 
 
 # Create a noisy audio dataset, which has the same folder tree with the original dataset, with given SNR value
+# Also copy transcript files to the same location as the original dataset to the noisy dataset
 def make_noisy_dataset(audio_dataset_dir, noise_dataset_dir, destination_dir
                        , file_type, snr):   
+    
     # Create empty folders mimicing the folder tree of audio dataset
     try: 
         current_dir = os.getcwd()
@@ -34,33 +36,50 @@ def make_noisy_dataset(audio_dataset_dir, noise_dataset_dir, destination_dir
         noise = AudioSegment.from_file(noise_dataset_dir)
         os.chdir(os.path.dirname(os.path.abspath(audio_dataset_dir)))
         _make_noisy_audio(audio_dataset_folder, audio_filename, destination_dir, file_type, noise, snr)
+        _copy_transcript(audio_dataset_folder, audio_filename, destination_dir, '*.txt')
+        _copy_transcript(audio_dataset_folder, audio_filename, destination_dir, '*.TXT')
         os.chdir(current_dir)
     
 
 # Helper function to copy empty folders of audio dataset into destination
 def _ig_f(dir, files):
+    
     return [f for f in files if os.path.isfile(os.path.join(dir, f))]
+
+# Copy transcript file to the same location as the original dataset to the noisy dataset
+def _copy_transcript(audio_dataset_folder, audio_filename, destination, file_type):
+    
+    # Search for files inside the folder
+    for entry in os.listdir(audio_dataset_folder):
+        
+        # Check if the files have "file_type"
+        if fnmatch.fnmatch(entry, file_type):
+            # Copy the transcript file
+            shutil.copy(audio_dataset_folder + '/' + entry, destination + audio_filename[:-4] + '/snr' + str(snr) + '/' + audio_dataset_folder + '/' + entry)
+            
+        elif os.path.isdir(audio_dataset_folder + '/' + entry):
+            # Recursively search for a transcript file and copy it
+            _copy_transcript(audio_dataset_folder + '/' + entry, audio_filename, destination_dir, file_type)
 
 
 # Recursively search audio files with file_type argument, and then copy the audio with noise added into the destination
 def _make_noisy_audio(audio_dataset_folder, audio_filename, destination, file_type, noise, snr):
+    
     # Search for files inside the folder
     for entry in os.listdir(audio_dataset_folder):
+        
         # Check if the files have "file_type"
         if fnmatch.fnmatch(entry, file_type):
             print('Adding noise to ' + entry)
             sound = AudioSegment.from_file(audio_dataset_folder + '/' + entry)
-            
             snr_in_dB = 20*np.log10(sound.rms / noise.rms)
-            
             # Pick a random interval of the noise because the noise is longer
             noise_start_time = np.random.rand()*(noise.duration_seconds - sound.duration_seconds)
-            
             # Combine audio and noise with specified SNR value
             combined = sound.overlay(noise[1e3*noise_start_time:], gain_during_overlay=-snr_in_dB + float(snr))
-            
             # Create a noisy audio
             combined.export(destination + audio_filename[:-4] + '/snr' + str(snr) + '/' + audio_dataset_folder + '/' + entry, format = file_type[2:])
+            
         elif os.path.isdir(audio_dataset_folder + '/' + entry):
             # Recursively search for an audio and create a noisy audio
             _make_noisy_audio(audio_dataset_folder + '/' + entry, audio_filename, destination_dir, file_type, noise, snr)
